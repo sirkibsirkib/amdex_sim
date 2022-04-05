23236cbffc0f739ba9c7711aa3dca17b31658456
sirkibsirkib <christopher.esterhuyse@gmail.com>
2022-04-05 14:55:23 +0200

    ye

--- main.py
+++ main.py
@@ -1,64 +1,71 @@
 import sys
 import site_config
-from pathlib import Path
+# from pathlib import Path
 
-from inspect import getargspec, getmembers, isfunction
+# from inspect import getargspec, getmembers, isfunction
 
 
 class Simulator:
     def __init__(self):
-        pass
+        self.messages = []
+        self.states = dict()
+        for name, store, handler in site_config.sites:
+            self.states[name] = SiteState(store, handler)
 
-    def send(self, dest, msg):
-        pass
+    def site_exists(self, dest):
+        return dest in self.states
+
+    def send_msg(self, dest, msg):
+        if self.site_exists(dest):
+            self.messages.append((dest, msg))
+            return True
+        return False
+
 
 PREFIX = "handler_"
 PREFIX_LEN = len(PREFIX)
 
 class SiteState:
-    def __init__(self, handler):
+    def __init__(self, store, handler):
+        self.store = store
         self.handler = handler
-        self.store = dict()
-
-def initialize_states():
-    states = dict()
-    for name, member in getmembers(site_config, isfunction):
-        if name.startswith(PREFIX) and len(getargspec(member)[0]) == 2:
-            states[name[PREFIX_LEN:]] = SiteState(member)
-    return states
 
 
 def main():
-    states = initialize_states()
-    print("has states for: {}".format(set(states.keys())))
-    msg_list = []
+    simulator = Simulator()
+    print("has states for: {}".format(set(simulator.states.keys())))
     while True:
         print("available messages: [")
-        for i, (dest, msg) in enumerate(msg_list):
+        for i, (dest, msg) in enumerate(simulator.messages):
             print("  [{}] @ {} {}".format(i, dest, msg))
         print("]")
 
         got = input("Next message to receive: ").split(" ")
         print("got", got)
-        if len(got) == 3 and got[0] == "@":
-            _, dest, msg = got
-            if dest in states:
-                print('sending to {} message {}'.format(dest, msg))
-                msg_list.append((dest, eval(msg)))
-            else:
-                print("no state for dest '{}'".format(dest))
-        elif len(got) == 1:
+        if not got:
+            continue
+        elif got[0] == '!':
+            # send
+            try: dest, msg = got[1:3]
+            except: print("WHEE")
+            try: assert(simulator.send_msg(dest, msg))
+            except: print("WUH")
+        elif got[0] == '?':
             try:
-                index = int(got[0])
-                dest, msg = msg_list.pop(index)
-            except ValueError:
-                print("tried to interpret that as an (integer) index of the msg to receive")
-                continue
-            except IndexError:
-                print("no message to receive with that index")
-                continue
-            state = states[dest]
-            state.handler(state.store, msg)
+                index = int(got[1])
+                dest, msg = simulator.messages.pop(index)
+                state = simulator.states[dest]
+                state.handler(simulator, state.store, msg)
+                print('{} handled msg {}\n'.format(dest, msg))
+            except IndexError: print("WEH")
+        elif got[0] == "@":
+            try:
+                dest = got[1]
+                state = simulator.states[dest]
+                print(state.store)
+            except: print("WAR")
+        else: print("WHAT??")
+
 
 if __name__ == "__main__":
     main()
--- site_config.py
+++ site_config.py
@@ -1,5 +1,10 @@
+def handler_reasoner(network, store, msg):
+	network.send_msg('bob', msg)
+def handler_bob(network, store, msg):
+	pass
 
-def foo(): pass
 
-def handler_amy(store, msg):
- 	print("howdy from amy!")
+sites = [
+	("reasoner", dict(), handler_reasoner),
+	("bob", dict(), handler_bob),
+]
